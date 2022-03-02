ad43ff5ab81200f9fd15d44f3a46565467330797
katatonia <pmilekhina@gmail.com>
2022-03-02 18:59:19 +0300

    Меняет логику и имя функции

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 const getRandomInt = (min, max) => {
   if (max <= min) {
-    return ('Неверный диапазон. Максимальное число не может быть меньше или равно минимальному.')
+    throw new Error('Неверный диапазон. Максимальное число не может быть меньше или равно минимальному.');
   } else {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-}
+};
 
 getRandomInt(0, 10);
 
-const getRandomDecimal = (min, max, fraction = 1) => {
+const getRandomFloat = (min, max, fraction = 1) => {
   if (max <= min) {
-    return ('Неверный диапазон. Максимальное число не может быть меньше или равно минимальному.');
+    throw new Error('Неверный диапазон. Максимальное число не может быть меньше или равно минимальному.');
   }
 
   const exponentiating = 10 ** fraction;
   return Math.floor((Math.random() * (max - min) + min) * exponentiating) / exponentiating;
-}
+};
 
-getRandomDecimal(0, 10);
+getRandomFloat(0, 10);
